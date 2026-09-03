527d9055bfbc6ffdb40ed56b2c92021d14cca78c
itamarboch <itamar.boch@gmail.com>
2026-09-03 20:25:25 +0300

    First version of the code

--- main.dart
+++ main.dart
@@ -1,1 +1,15 @@
+  import 'dart:convert';
+import 'dart:io';
+void main() async {
+  List<int> temperatures =[];
 
+  String fliePath = r"C:\Users\User\Desktop\Itamar's Projects\json\readings.json";
+  final file = File(fliePath);
+  final jsonString = await file.readAsString();
+  Map<String, dynamic>list = jsonDecode(jsonString);
+      int temp = list['temperature'] as int;
+
+  temperatures.add(temp);
+  double average = temperatures.reduce((a, b) => a + b) / temperatures.length;
+  print(average);
+}
