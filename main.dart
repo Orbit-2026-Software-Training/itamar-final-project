ecba0d6a7dbaab0c3ab867ecdd293bd7a57ed6d5
itamarboch <itamar.boch@gmail.com>
2026-09-07 17:44:29 +0300

    finished the stage by making a if function that counts the amount of times temp is bigger then 25

--- main.dart
+++ main.dart
@@ -1,15 +1,26 @@
-  import 'dart:convert';
+import 'dart:convert';
 import 'dart:io';
 void main() async {
-  List<int> temperatures =[];
-
+  List<double> temperatures = [];
   String fliePath = r"C:\Users\User\Desktop\Itamar's Projects\json\readings.json";
   final file = File(fliePath);
   final jsonString = await file.readAsString();
-  Map<String, dynamic>list = jsonDecode(jsonString);
-      int temp = list['temperature'] as int;
+    
+     List<dynamic> list = jsonDecode(jsonString);
+    /* iterate over the list and add the temperature to the temperatures list */
+      for(var item in list) {
+        double temp  = (item['temperature'] as num).toDouble();
+        temperatures.add(temp);
+      }
+      /* find the average temperature and the number of times the temperature was bigger than 25 */
+    int count_times_bigger_then_25 = 0;
+    for(double temp in temperatures) {
+      if(temp > 25) {
+        count_times_bigger_then_25++;
+      }
+    }
 
-  temperatures.add(temp);
   double average = temperatures.reduce((a, b) => a + b) / temperatures.length;
   print(average);
+  print(count_times_bigger_then_25);
 }
