9390296249d61e1593ef0aa59a62682e86d9af62
itamarboch <itamar.boch@gmail.com>
2026-09-07 17:56:10 +0300

    changed the comment

--- main.dart
+++ main.dart
@@ -1,24 +1,29 @@
 import 'dart:convert';
 import 'dart:io';
+
 void main() async {
   List<double> temperatures = [];
-  String fliePath = r"C:\Users\User\Desktop\Itamar's Projects\json\readings.json";
+  String fliePath =
+      r"readings.json";
+      
   final file = File(fliePath);
   final jsonString = await file.readAsString();
-    
-     List<dynamic> list = jsonDecode(jsonString);
-    /* iterate over the list and add the temperature to the temperatures list */
-      for(var item in list) {
-        double temp  = (item['temperature'] as num).toDouble();
-        temperatures.add(temp);
-      }
-      /* find the average temperature and the number of times the temperature was bigger than 25 */
-    int count_times_bigger_then_25 = 0;
-    for(double temp in temperatures) {
-      if(temp > 25) {
-        count_times_bigger_then_25++;
-      }
+
+  List<dynamic> list = jsonDecode(jsonString);
+
+  // Loop through the list and add the temperature to the list that has the temperatures as a double 
+  for (var item in list) {
+    double temp = (item['temperature'] as num).toDouble();
+    temperatures.add(temp);
+  }
+
+  // finds the average temperature and the number of times the temperature was bigger than 25 */
+  int count_times_bigger_then_25 = 0;
+  for (double temp in temperatures) {
+    if (temp > 25) {
+      count_times_bigger_then_25++;
     }
+  }
 
   double average = temperatures.reduce((a, b) => a + b) / temperatures.length;
   print(average);
